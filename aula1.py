6a6790eda0474484432929976702931a2fcbcabb
Nicolas Martins Souza <chinho2007@hotmail.com>
2024-06-14 22:51:10 -0300

    .

---
+++ aula1.py
@@ -0,0 +1,8 @@
+''' DocString type 1 '''
+""" DocString type 2 """
+# Comentario #
+
+# Comentario abaixo
+print(123) # Na frente
+# Abaixo
+print(456)
---
+++ aula3.py
@@ -0,0 +1,27 @@
+""" 
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+
+"""
+''' 
+ATALHOS
+
+Selecionar linha + Ctrl X -> CTRL + V
+'''
+print(1234) # Python recebe informações de forma direta
+print(1, 'Luiz "Otávio"')
+print(2, "Luiz 'Otávio'")
+
+# Aspas simples
+print('Luiz Otávio')
+
+# Aspas duplas
+print("Luiz Otávio")
+
+# Escape
+print("Luiz \"Otávio\"")
+
+# r
+print(r"Luiz \"Otávio\"")
